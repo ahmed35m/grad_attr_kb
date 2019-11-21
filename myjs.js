b3e25990e53320d152b70c3381e5d26297e7f312
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2019-11-21 01:07:29 -0600

    Hooked event trigger to modals

--- myjs.js
+++ myjs.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 	responseHandler(fakeGetRequest());
 	//createGA();
-	renderAddOption();
+	
 };
 
 
@@ -28,6 +28,9 @@ function responseHandler(resp) {
 
 
 	});
+	if (editAcces()){
+	renderAddOption(); }
+
 }
 
 
@@ -44,14 +47,16 @@ function createSubGA(data) {
 	var ga_title = document.createElement('label');
 	var ga_label = document.createElement('label');
 
+	if (editAcces()){
 	var ga_edit = document.createElement('div');
 	var ga_icon = document.createElementNS("http://www.w3.org/2000/svg", "textpath");
 	ga_icon = $('<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>');
-
+	$(ga_edit).addClass('edit_box').append(ga_icon);
+	}
 	$(ga_instance).attr('id', data.number).attr('name', 'grad_attr').addClass(' grad_attr');
 	$(ga_n).text(data.number).addClass('sub_n');
 	$(ga_title).text(data.title).addClass('sub_title');
-	$(ga_edit).addClass('edit_box').append(ga_icon);
+	
 
 
 	$(ga_label).addClass('sub_ga_label').append(ga_n).append(ga_title);
@@ -77,31 +82,38 @@ function createGA(numb, title) {
 	var ga_instance = document.createElement('div');
 	var ga_n_label = document.createElement('label');
 	var ga_title = document.createElement('div');
-	var ga_label = document.createElement('label');
+	var ga_label = document.createElement('div');
 	var sub_ga = document.createElement('div');
 	var ga_edit = document.createElement('div');
-	var ga_add = document.createElement('div');
+	var ga_a = document.createElement('div');
+	var ga_e = document.createElement('div');
+	var d = new Object();
+	if (editAcces()){
 	var ga_add_icon = document.createElementNS("http://www.w3.org/2000/svg", "textpath");
-	var ga_icon = document.createElementNS("http://www.w3.org/2000/svg", "textpath");
+	var ga_edit_icon = document.createElementNS("http://www.w3.org/2000/svg", "textpath");
+	ga_edit_icon = $('<div class="add_sub_btn" id="'+numb+'_edit" ><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg></div>');
+	ga_add_icon = $('<div class="edit_ga_btn" id="'+numb+'_add"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg></div>');
+	$(ga_a).append(ga_add_icon);
+	$(ga_e).append(ga_edit_icon);
+	ga_a.addEventListener('click', function(){renderModalAdd();});
+	d.title = title;
+	d.numb = numb;
+	ga_e.addEventListener('click', function(){renderModalEdit(d);});
 
-	ga_icon = $('<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>');
-
-	ga_add_icon = $('<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>');
-	$(ga_edit).append(ga_add_icon);
-
+	$(ga_edit).append(ga_a);	
+	}
 	$(ga_instance).attr('id', numb).attr('name', 'grad_attr').addClass('grad_attr');
 	$(ga_n_label).text(numb).addClass('label_numb');
 	$(ga_title).text(title).addClass('label_title');
-	$(ga_edit).addClass('edit_box').append(ga_icon);
+	$(ga_edit).addClass('edit_box').append(ga_a).append(ga_e);
 	$(ga_label).addClass('ga_label');
 
 	$(sub_ga).addClass('sub_ga');
-	ga_instance.addEventListener('click', function(){ toggleVisibility(this) } );
+	ga_label.addEventListener('click', function(){ toggleVisibility(this) } );
 	$(ga_label).append(ga_n_label).append(ga_title);
 	$(ga_instance).append(ga_label).append(ga_edit);
 	$(wrapper).append(ga_instance).append(sub_ga);
 	$("#GA_MASTER").append(wrapper);
-
 	console.log('Grad Attr Instance created');
 
 };
@@ -119,49 +131,67 @@ function renderAddOption() {
 
 
 function renderModalAdd() {
-	/*
-	var wrapper = document.createElement('div');
-	var container= document.createElement('div');
-	var textnode = document.createTextNode('Add a Graduate Attribute');
-	var save = document.createElement('button');
-	var close = document.createElement('button');
 
-	var form = $('<form method="POST" id="add_form"> </form>');
-	var textinput = $('<input type="text" id="add_input value="Enter New Name" /">');
-		var textinput =  document.createElement('input'); //$('<form method="POST"> <input class="ga_input"/> </form>');
-	
-	//$(textnode).text('Add a Graduate Attribute').addClass('modal_heading02');
-	$(textinput).addClass('modal_input');
-	$(save).addClass('modal_btn').text('Save');
-	$(close).addClass('modal_btn').text('Close');
-	$(form).append(textinput).append(save).append(close);
-	//save.addEventListener('onclick',function (){alert('GA Added!');});
-	
-	$(container).append(textnode).append(form).addClass('modal_container');
-	$(wrapper).append(container);
-	*/
-
-	var modal = $('<div class="modal" tabindex="-1" role="dialog"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="Add a Graduate Attribute">Add a Graduate Attribute</h5>  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">  <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">	</div><div class="modal-footer"> <button type="button" class="btn btn-primary">Save changes</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div> </div>');
+	$('.modal').remove();
+	var modal = $(`<div class="modal" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">
+		<h5 class="modal_Add">Add a New Graduate Attribute</h5>  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span></button></div><div class="modal-body">  
+			<label>New Attribute:</label>
+			<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" >	
+			</div>
+			<div class="modal-footer"> 
+			<button type="button" class="btn btn-primary">Add</button>
+		<button type="button" class="btn btn-secondary" data-dismiss="modal">Discard</button></div></div></div> </div>`);
 	$(modal).attr('id', 'myModal');
 
 	// var test = $('<!-- Modal -->  <div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>          <h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>	</div>');
 	$('#GA_MASTER').append(modal);
+	$('#myModal').modal({focus: true});
+
+}
+
+
+function renderModalEdit(data) {
+
+	$('.modal').remove();
+	var modal = $(`<div class="modal" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">
+		<h5 class="modal_Add">Modify Graduate Attribute</h5>  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span></button></div><div class="modal-body">  
+			<label>Attribute Title:</label>
+			<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value="`+data.title+`">	
+			</div>
+			<div class="modal-footer override_ftr" > 
+			<div><button type="button" class="btn btn-outline-danger">Delete</button></div>
+			<div><button type="button" class="btn btn-primary">Save</button>
+		<button type="button" class="btn btn-secondary" data-dismiss="modal">Discard</button></div></div></div></div> </div>`);
+	$(modal).attr('id', 'myModal');
+
+	// var test = $('<!-- Modal -->  <div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>          <h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>	</div>');
+	$('#GA_MASTER').append(modal);
+	$('#myModal').modal({focus: true});
 
 }
 
 
 function toggleVisibility(ref){
-	var sub_ga = $(ref).siblings('.sub_ga');
+	console.log('Visibilit clicked');
+	var sub_ga = $(ref).parent().siblings('.sub_ga');
 	$(sub_ga).toggleClass('hidden');
 
 
 }
 
-
+function editAcces(){
+	let resp = fakeGetRequest();
+	return (resp.access>=3 ? true : false);
+}
 
 function fakeGetRequest() {
 
 	var resp = {
+		access: 3,
 		"GA": [{
 				"id": "",
 				"number": 1,
